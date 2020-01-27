06f74d5052ddb98673897800d09070beb0081de0
Eun Kang <choa030@gmail.com>
2020-01-27 22:35:51 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// google csv url
+var csv =
+  "https://docs.google.com/spreadsheets/d/1gYzw56_7EyoKAitdRL-_5j2jCmXorbgoUnZTimuqBWc/edit?usp=sharing";
+
+function init() {
+  Tabletop.init({ key: csv, callback: showInfo, simpleSheet: true }); //multiple sheet should be changed
+}
+
+function showInfo(d, tabletop) {
+  const container = document.querySelector(".title");
+  for (let i = 0; i < d.length; i++) {
+    let createDiv = document.createElement("div");
+    createDiv.className = "section";
+    createDiv.innerHTML = `
+    <h5 class="pub pu_title">${d[i].Title}</h5>
+    <p class="pub author">${d[i].Author}</p>
+    <h6 class="pub publisher">${d[i].Publisher}</h6>`;
+    container.appendChild(createDiv);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", init);
