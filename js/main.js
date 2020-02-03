5e3446d1374e06debe9240dbc6ef7e668c10fcc6
Eun Kang <choa030@gmail.com>
2020-02-03 16:01:54 +0900

    0203

--- js/main.js
+++ js/main.js
@@ -2,22 +2,41 @@
 var csv =
   "https://docs.google.com/spreadsheets/d/1gYzw56_7EyoKAitdRL-_5j2jCmXorbgoUnZTimuqBWc/edit?usp=sharing";
 
+var field_list = ["Poster", "Workshop", "Conference", "Journal"];
+
 function init() {
-  Tabletop.init({ key: csv, callback: showInfo, simpleSheet: true }); //multiple sheet should be changed
+  Tabletop.init({ key: csv, callback: showInfopub, simpleSheet: true }); //multiple sheet should be changed
 }
 
 // TODO: Filtering according to the fields, seperate by date
 
-function showInfo(d, tabletop) {
-  const container = document.querySelector(".title");
+function showInfopub(d, tabletop) {
+  const container = document.querySelector(".publications");
+  const container_pub = document.querySelector(".projects");
   for (let i = 0; i < d.length; i++) {
-    let createDiv = document.createElement("div");
-    createDiv.className = "section";
-    createDiv.innerHTML = `
-    <h5 class="pub pu_title">${d[i].Title}</h5>
-    <p class="pub author">${d[i].Author}</p>
-    <h6 class="pub publisher">${d[i].Publisher}</h6>`;
-    container.appendChild(createDiv);
+    if (field_list.indexOf(d[i].Field) > -1) {
+      let createDiv = document.createElement("div");
+      createDiv.className = "section";
+      createDiv.innerHTML = `
+      <h6 class="pub pu_title">${d[i].Title}</h6>
+      <div class="pub author">${d[i].Author}</div>
+      <div class="pub publisher">${d[i].Publisher}</div>
+      <a class="pub link" href="${d[i].Paper ? d[i].Paper : null}">${
+        d[i].Paper ? "&#128218;" : " "
+      }</a>`;
+      container.appendChild(createDiv);
+    } else {
+      let createDiv = document.createElement("div");
+      createDiv.className = "section";
+      createDiv.innerHTML = `
+      <h6 class="pub pu_title">${d[i].Title}</h6>
+      <div class="pub author">${d[i].Author}</div>
+      <div class="pub publisher">${d[i].Publisher}</div>
+      <a class="pub link" href="${d[i].Paper ? d[i].Paper : null}">${
+        d[i].Paper ? "paper" : " "
+      }</a>`;
+      container_pub.appendChild(createDiv);
+    }
   }
 }
 
---
+++ js/message.js
@@ -0,0 +1,12 @@
+function intro() {
+  const message = "Hi, Nice to meet you :) ";
+  const container = [];
+  const div = document.querySelector("introduction");
+  for (let i = 0; i < message.length; i++) {
+    container.push(message[i]);
+    div.appendChild(container);
+    console.log(container);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", intro);
